8d513eb0a1a25a6e8e1e8bc503c326cd39392e5b
unxok <anderluke707@gmail.com>
2024-03-18 01:58:43 -0700

    started working on left panel

--- src/components/CenterPanelGroup/LeftPanel/index.tsx
+++ src/components/CenterPanelGroup/LeftPanel/index.tsx
@@ -1,7 +1,26 @@
-import { ResizablePanel } from "@/components/ui/resizable";
+import { ResizablePanelSpacer } from "@/components/ResizablePanelSpacer";
+import { Button } from "@/components/ui/button";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 
 export const LeftPanel = () => (
   <ResizablePanel defaultSize={15} minSize={5} collapsible collapsedSize={0}>
-    Left
+    <ResizablePanelGroup
+      id={"left-panel"}
+      autoSaveId={"left-panel"}
+      direction="vertical"
+    >
+      <ResizablePanelSpacer />
+      <ResizableHandle className="bg-transparent" />
+      <ResizablePanel className="flex flex-col items-center justify-start gap-5">
+        <Button>Add board</Button>
+        <Button variant={"secondary"}>Create view</Button>
+      </ResizablePanel>
+      <ResizableHandle className="bg-transparent" />
+      <ResizablePanelSpacer />
+    </ResizablePanelGroup>
   </ResizablePanel>
 );
